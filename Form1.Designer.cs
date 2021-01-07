c5fe50fd72944b588dba755e413ec030c454bbb6
yu <chu2205ssd@gmail.com>
2021-01-07 21:18:53 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace _20201210
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnGo = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Btn2 = new System.Windows.Forms.Button();
+            this.TB1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnGo
+            // 
+            this.BtnGo.Font = new System.Drawing.Font("UD Digi Kyokasho NP-B", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.BtnGo.Location = new System.Drawing.Point(520, 89);
+            this.BtnGo.Name = "BtnGo";
+            this.BtnGo.Size = new System.Drawing.Size(105, 62);
+            this.BtnGo.TabIndex = 0;
+            this.BtnGo.Text = "抽圖";
+            this.BtnGo.UseVisualStyleBackColor = true;
+            this.BtnGo.Click += new System.EventHandler(this.BtnGo_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(473, 449);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Btn2
+            // 
+            this.Btn2.Font = new System.Drawing.Font("UD Digi Kyokasho NP-B", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Btn2.Location = new System.Drawing.Point(520, 468);
+            this.Btn2.Name = "Btn2";
+            this.Btn2.Size = new System.Drawing.Size(105, 59);
+            this.Btn2.TabIndex = 3;
+            this.Btn2.Text = "輸入";
+            this.Btn2.UseVisualStyleBackColor = true;
+            this.Btn2.Click += new System.EventHandler(this.Btn2_Click);
+            // 
+            // TB1
+            // 
+            this.TB1.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.TB1.Location = new System.Drawing.Point(315, 492);
+            this.TB1.Name = "TB1";
+            this.TB1.Size = new System.Drawing.Size(170, 35);
+            this.TB1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("UD Digi Kyokasho NP-B", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(520, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 66);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "下載";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 575);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TB1);
+            this.Controls.Add(this.Btn2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnGo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnGo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Btn2;
+        private System.Windows.Forms.TextBox TB1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace _20201210
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class resp
+        {
+            public List<img> data { get; set; }
+        }
+
+        public class img
+        {
+            public string link { get; set; }
+        }
+
+        private resp GetImages(string albumHash, string clientId)
+        {
+            resp result = null;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"https://api.imgur.com/3/album/{albumHash}/images");
+                //Add Header 
+                WebHeaderCollection webHeaderCollection = request.Headers;
+                webHeaderCollection.Add("Authorization", $"Client-ID {clientId}");
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                string json = readStream.ReadToEnd();
+                result = JsonConvert.DeserializeObject<resp>(json);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.ToString());
+                throw;
+            }
+            return result;
+        }
+
+        Image img1;
+
+        private void BtnGo_Click(object sender, EventArgs e)
+        {
+            var m = GetImages("G1MkeJL", "b29cb06c0adff39");
+            Random crandom = new Random();
+            int x = crandom.Next(0, 10);
+
+            if (m == null) 
+            {
+                return;
+            }
+            //json --> object 
+            string url = m.data[x].link;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream receiveStream = response.GetResponseStream();
+            img1 = System.Drawing.Image.FromStream(receiveStream);
+
+            Console.WriteLine(img1.Width);
+            pictureBox1.Image = img1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            
+            Bitmap bmp = (Bitmap)img1;
+            int a = bmp.Width / 10 ;
+            RectangleF rectf = new RectangleF(0, 0, bmp.Width, bmp.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            
+            g.DrawString(TB1.Text, new Font("UD Digi Kyokasho NP-B", a), Brushes.White, rectf);
+
+            g.Flush();
+
+            pictureBox1.Image = bmp;
+
+
+
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|PnG Image|*.png|Wmf  Image|*.wmf";
+            saveFileDialog1.FilterIndex = 0;
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("沒有圖片啊");
+            }
+            else if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (pictureBox1.Image != null)
+                {
+                    pictureBox1.Image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+            }
+        }
+    }
+}
